cc8be82ae42375aed3dd7b54f7fc2d5560e2103b
Felipe Fidalgo <felipe_apsf@hotmail.com>
2020-12-12 12:55:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp.Conta/Entities/Account.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp.Conta.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp.Conta.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string name, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Name = name;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
